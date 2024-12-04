44eca6ff8f00375a28f877706660ed9ebfb1e991
Francesco Puppo <francescopuppo@me.com>
2024-12-05 00:09:25 +0100

    Another RSS fix + Open external links in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import rss from "@astrojs/rss";
+import { targetBlank } from "./src/plugins/targetBlank";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://iamfran.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    rehypePlugins: [[targetBlank, { domain: "yourdomain.com" }]],
+  },
 });
---
+++ src/plugins/targetBlank.ts
@@ -0,0 +1,20 @@
+// src/plugins/targetBlank.ts
+
+import type { RehypePlugin } from "@astrojs/markdown-remark";
+import { visit } from "unist-util-visit";
+import type { Element } from "hast";
+
+export const targetBlank: RehypePlugin = ({ domain = "" } = {}) => {
+  return (tree) => {
+    visit(tree, "element", (e: Element) => {
+      if (
+        e.tagName === "a" &&
+        e.properties?.href &&
+        e.properties.href.toString().startsWith("http") &&
+        !e.properties.href.toString().includes(domain)
+      ) {
+        e.properties!["target"] = "_blank";
+      }
+    });
+  };
+};
