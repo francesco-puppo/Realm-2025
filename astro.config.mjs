c765b88d4c21ad64171da761fa881da23c4334c0
Francesco Puppo <francescopuppo@me.com>
2024-12-05 07:35:50 +0100

    reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import rss from "@astrojs/rss";
-import { targetBlank } from "./src/plugins/targetBlank";
 
 export default defineConfig({
-  site: "https://iamfran.netlify.app",
+  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    rehypePlugins: [[targetBlank, { domain: "yourdomain.com" }]],
-  },
 });
