53e54ae35f3b4e52e1696f358d61d4ccb76f9fb3
Francesco Puppo <francescopuppo@me.com>
2024-12-07 14:41:17 +0100

    fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Fran K",
+  NAME: "Fran",
   EMAIL: "hey@iamfran.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
