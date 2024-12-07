259afdbb2371d1b2f38799a023519cf8d91effa2
Francesco Puppo <francescopuppo@me.com>
2024-12-07 14:39:12 +0100

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import embeds from "astro-embed/integration";
 
 export default defineConfig({
   site: "https://realm-2025.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [embeds(), mdx(), sitemap(), tailwind()],
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,23 +3,24 @@ import { getCollection } from "astro:content";
 import { HOME } from "@consts";
 
 type Context = {
-  site: string
-}
+  site: string;
+};
 
 export async function GET(context: Context) {
-  const blog = (await getCollection("blog"))
-  .filter(post => !post.data.draft);
+  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
 
-  const projects = (await getCollection("projects"))
-    .filter(project => !project.data.draft);
+  const projects = (await getCollection("projects")).filter(
+    (project) => !project.data.draft
+  );
 
-  const items = [...blog, ...projects]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+  const items = [...blog, ...projects].sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
+  );
 
   return rss({
     title: HOME.TITLE,
     description: HOME.DESCRIPTION,
-    site: context.site,
+    site: "Fran's Realm",
     items: items.map((item) => ({
       title: item.data.title,
       description: item.data.description,
