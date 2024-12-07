65df2f142ba5d86a287802e9936df864d0a1a6dc
Francesco Puppo <francescopuppo@me.com>
2024-12-07 15:11:02 +0100

    open external links in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import embeds from "astro-embed/integration";
+import { externalAnchorPlugin } from "/src/plugins/external-anchor-plugin.mjs";
 
 export default defineConfig({
   site: "https://realm-2025.vercel.app",
   integrations: [embeds(), mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [externalAnchorPlugin],
+  },
 });
