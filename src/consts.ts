5b1c34a519b7c4d9bb16045278fbdc2600bb3462
Francesco Puppo <francescopuppo@me.com>
2024-12-08 11:38:39 +0100

    small RSS fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,51 +1,51 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Fran",
-  EMAIL: "hey@iamfran.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+	NAME: "Fran",
+	EMAIL: "hey@iamfran.com",
+	NUM_POSTS_ON_HOMEPAGE: 3,
+	NUM_WORKS_ON_HOMEPAGE: 2,
+	NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+	TITLE: "Fran's Realm",
+	DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+	TITLE: "Blog",
+	DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+	TITLE: "Work",
+	DESCRIPTION: "Where I have worked and what I have done.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects, with links to repositories and demos.",
+	TITLE: "Projects",
+	DESCRIPTION:
+		"A collection of my projects, with links to repositories and demos.",
 };
 
 export const ABOUT: Metadata = {
-  TITLE: "About",
-  DESCRIPTION:
-    "A collection of my projects, with links to repositories and demos.",
+	TITLE: "About",
+	DESCRIPTION:
+		"A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
-  {
-    NAME: "bluesky",
-    HREF: "https://bsky.app/profile/iamfran.bsky.social",
-  },
-  // {
-  //   NAME: "rss",
-  // HREF: "https://realm-2025.vercel.app/rss.xml",
-  // },
-  {
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/francescopuppo/",
-  },
+	{
+		NAME: "bluesky",
+		HREF: "https://bsky.app/profile/iamfran.bsky.social",
+	},
+	// {
+	//   NAME: "rss",
+	// HREF: "https://realm-2025.vercel.app/rss.xml",
+	// },
+	{
+		NAME: "linkedin",
+		HREF: "https://www.linkedin.com/in/francescopuppo/",
+	},
 ];
